abd70e33972acab2b55cd2a47abfd5c05c2d8aa0
Sam Sliman <smsliman@aol.com>
2020-07-01 09:55:14 -0500

    Feautre/google sheets api (#11)
    * "ope" feature first attempt
    
    * ope function fixes
    
    * regex as string fix
    
    * remove ds_store
    
    * last lil fix
    
    * lower -> lower()
    
    * it works now!
    
    * Update ope.csv
    
    * Update bot.py
    
    * test
    
    * test
    
    * test
    
    * test
    
    * google sheets api refactor
    
    * dum dum fix
    
    * dump creds to file from env
    
    Co-authored-by: Sam Sliman <samsliman@Sams-MBP.attlocal.net>
    Co-authored-by: Aadi Bajpai <me@aadibajpai.me>

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import asyncio
+import base64
 import csv
 import discord
+import gspread
+import json
+from oauth2client.service_account import ServiceAccountCredentials
 import os
 import re
 from datetime import datetime, timedelta
@@ -9,10 +13,43 @@ from discord.ext import commands, tasks
 
 bot = commands.Bot(command_prefix='v;')
 
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds_string = json.loads(base64.b64decode(os.environ["GOOGLE_API_CREDS"] + "==="))
+with open('gcreds.json', 'w') as fp:
+    json.dump(creds_string, fp)
+creds = ServiceAccountCredentials.from_json_keyfile_name('gcreds.json', scope)
+client = gspread.authorize(creds)
+sheet = client.open("Data").sheet1
+
+
 
 target_channel_id = 702296171829264394  # wellness-office in vandy discord
 # target_channel_id = 722609125950750771 # testing
 activity = True
+@bot.event
+async def on_message(message):
+    if not message.author.bot:
+        print(message.author)
+        match = re.search(r'\bope\b', message.content.lower())
+        if match:
+            sheet = client.open("Data").sheet1
+            data = sheet.get_all_records()
+            author_found = False
+            author_count = 0
+            index = 0
+            new_index = 2
+            for record in data:
+                if message.author.id == record['id']:
+                    author_found = True
+                    sheet.update_cell(record['index'] + 1, 2, record['number'] + 1)
+                    author_count = record['number'] + 1
+                new_index += 1
+            if not author_found:
+                newRow = [str(message.author.id), 1, new_index-1]
+                sheet.insert_row(newRow, new_index)
+                author_count=1
+            await message.channel.send(f'{message.author} has said Ope {author_count} times. Yikes.')
+    await bot.process_commands(message)
 
 @bot.event
 async def on_message(message):
